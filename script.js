44c646ebac2d550922a036875918890917aef6b8
Rodrigo Domingues <91419413+Rodrigo0206@users.noreply.github.com>
2022-07-07 14:22:10 -0300

    fibonacci com mensagem

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
+var numero = 13;
 var i;
 var fib = [0, 1]; 
 
 for (i = 2; i <= 10; i++) {
  
   fib[i] = fib[i - 2] + fib[i - 1];
+
   console.log(fib[i]);
+    if (fib[i] === numero ){
+        console.log('o numero acima foi previamente informado e pertence ao fibonacci');
+    }
 }
-console.log('o numero informado é 13');
-console.log('o numero informado pertence ao fibonacci');
+//console.log('o numero informado é 13');
+//console.log('o numero informado pertence ao fibonacci');
