3bffe1d4c0cf1e00dd13775d46badb2497fd48d4
Rodrigo Domingues <91419413+Rodrigo0206@users.noreply.github.com>
2022-08-26 12:53:13 -0300

    desadio5 completo

---
+++ desafio5.js
@@ -0,0 +1,9 @@
+function inverter(string) {
+    var saida = "";
+    for (var i=string.length-1;i>=0;i--)  {
+        saida+= string[i]
+    }
+    console.log(saida);
+  }
+  inverter("rodrigo");
+
