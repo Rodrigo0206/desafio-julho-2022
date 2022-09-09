2f817e144de0449fa666f080ed3db39bdae92564
Rodrigo Domingues <91419413+Rodrigo0206@users.noreply.github.com>
2022-09-09 11:17:34 -0300

    faturamento maior e menor

---
+++ desaio3.1.js
@@ -0,0 +1,80 @@
+
+   
+gerarRelatorioFaturamentoPorDia();
+
+function gerarRelatorioFaturamentoPorDia(){
+
+    lerArquivoJson("data.json").then(faturamento => {
+
+        mostrarFaturamentoDia(faturamento);
+        console.log("terminou");
+    });
+
+    
+}
+
+function mostrarFaturamentoDia(faturamento){
+
+    let total = 0;
+    let quantidade =0;
+    let menorValor = 0;
+    let maiorValor = 0;
+
+
+    faturamento["faturamento"].forEach(obj => {
+
+        let dia = obj["dia"];
+        let faturamentoDiario = obj["faturamentoDiario"];
+        
+        console.log(`${dia} ${faturamentoDiario}`);
+        if(faturamentoDiario > 0){
+            total += faturamentoDiario;
+            quantidade++;
+        }
+            
+      if(faturamentoDiario > 0){
+        if (menorValor == 0) {
+        menorValor = faturamentoDiario;
+      } else {
+        if (faturamentoDiario < menorValor ){
+            menorValor = faturamentoDiario
+        }
+      }
+      }
+      if(faturamentoDiario > 0){
+        if (maiorValor == 0) {
+        maiorValor = faturamentoDiario;
+      } else {
+        if (faturamentoDiario > maiorValor ){
+            maiorValor = faturamentoDiario
+        }
+      }
+      }
+        
+        console.log('-------------------');
+
+    });
+    const media = total/quantidade;
+    console.log(`Media : ${media.toFixed(2)}`);
+
+    console.log('-------------------');
+    console.log(`menorValor ${menorValor}`);
+    console.log(`maiorValor ${maiorValor}`);
+}
+
+async function  lerArquivoJson(arquivoJsonNome){
+   
+    const fsp = require('fs').promises;
+
+    return await fsp.readFile(arquivoJsonNome).then(data => {
+
+        return converterJsonParaFaturamento(data);
+        
+    });
+}
+
+function converterJsonParaFaturamento(data){
+    
+    const faturamento = JSON.parse(data);     
+    return faturamento;    
+}
